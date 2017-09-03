57650d9843d47378302a4a6834ef5d0a4b2fb81f
Christopher Peckover <christopher.peckover@gmail.com>
2017-09-03 02:23:30 -0400

    Update dataset

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,11 @@
 insert into CUSTOMER (id, user_id, address, payment_method) values (1, 1, '23 Crestwood Dr., Cambridge, ON, Canada', 'VISA');
+insert into CUSTOMER (id, user_id, address, payment_method) values (2, 2, '374 Tallwood Ave, Richmond Hill, NY, USA', 'AE');
+insert into CUSTOMER (id, user_id, address, payment_method) values (3, 3, '31 Ohio St, Concord, NH, USA', 'MASTERCARD');
+insert into CUSTOMER (id, user_id, address, payment_method) values (4, 4, '7440 W. Willow Ave, Kansas City, MO, USA', 'VISA');
+insert into CUSTOMER (id, user_id, address, payment_method) values (5, 5, '8546 Plumb Branch St, Long Beach, NY, USA', 'VISA');
+
 insert into DRIVER (id, user_id, car) values (1, 1, 'Honda Civic');
+insert into DRIVER (id, user_id, car) values (2, 2, 'Hyundai Accent');
+insert into DRIVER (id, user_id, car) values (3, 3, 'Audi A4');
+insert into DRIVER (id, user_id, car) values (4, 4, 'Hummer');
+insert into DRIVER (id, user_id, car) values (5, 5, 'Mini Cooper');
